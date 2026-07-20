184e4193eb6846837b99903381674ae77a061597
Md Arif <arif7862016aa@gmail.com>
2026-07-21 02:13:10 +0530

    push 5

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println("MAINNN");
+        System.out.println("MAIN");
         int a = 23;
         String pass = "adminsh123";
         System.out.println(a);
